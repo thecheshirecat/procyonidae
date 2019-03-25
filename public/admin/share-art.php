ec214cdb50b705cc7016f23cf700f7b416a8a2cb
Sergi Codina <sergi.codina@movetia.com>
2019-03-25 17:57:35 +0100

    Added reCaptcha from google, changed Favicon

--- public/admin/share-art.php
+++ public/admin/share-art.php
@@ -1,24 +1,26 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
+    // header('Access-Control-Allow-Origin: *');
     
     $imageSend = array(
         "recieved" => "OK"
     );
 
-    $mailMessage = 'Someone shared an image for the website!
-    <br>
-    Image Artist: '.$_POST["artist"].'
-    <br>
-    Image Title: '.$_POST["title"].'
-    <br>
-    Image URL: '.$_POST["image"].'
-    <br>
-    Image Source: '.$_POST["source"];
+    $mailMessage = '
+    <html>
+        <head></head>
+        <body>
+            <p>Someone shared an image for the website!</p>
+            <br>
+            <p>Image Artist: <b>'.$_POST["artist"].'</b></p>
+            <p>Image Title: <b>'.$_POST["title"].'</b></p>
+            <p>Image: <img src="'.$_POST["image"].'" alt="'.$_POST["title"].'" /></p>
+            <p>Image Source: '.$_POST["source"].'</p>
+        </body>
+    </html>';
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8';
 
-    $headers = 'Content-type: text/html; charset=utf-8' . "\r\n".
-        'From: shareart@raccoonjunkyard.com';
-
-    if(mail ( "sergi.codina@movetia.com" , "Shared art" , $mailMessage, $headers )) {
+    if(mail ( "shareart@raccoonjunkyard.com" , "New art shared" , $mailMessage, $headers )) {
         $imageSend = array(
             "recieved" => "OK"
         );
--- src/pages/ShareArt.js
+++ src/pages/ShareArt.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react'
 import { SHARE_ART, SHARE_ART_URL } from '../constants';
 import Button from '../components/Button';
 
+import ReCAPTCHA from 'react-google-recaptcha';
+
+const recaptchaRef = React.createRef();
+const TESTKEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+const KEY = "6Ld_4JkUAAAAAL__co5gAX6QtuEkKQDTyYQ9V-aY"
+
 class ShareArt extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            disabled: false,
+            disabled: true,
             imageArtist: '',
             imageTitle: '',
             imageUrl: '',
-            imageSource: ''
+            imageSource: '',
+            imageSent: null
         }
         this.props.changeSection(SHARE_ART)
         this.props.setCurrentPage(1)
@@ -18,70 +25,91 @@ class ShareArt extends Component {
     handleSubmit = (event) => {
         event.preventDefault()
         this.setState({
-            disabled: true
-        })
-        var data = `artist=${this.state.imageArtist}&title=${this.state.imageTitle}&image=${this.state.imageUrl}&source=${this.state.imageSource}`
-        fetch(SHARE_ART_URL, {
-            method: 'POST',headers: new Headers({
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }),
-            body: data
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            if(data.recieved === "OK") {
-                console.log("image recieved")
-            }
-            else {
-                console.log("error recieving image", data.message)
-            }
-            this.setState({
-                disabled: false
+            disabled: true,
+            imageSent: "SENDING"
+        }, () => {
+            var data = `artist=${this.state.imageArtist}&title=${this.state.imageTitle}&image=${this.state.imageUrl}&source=${this.state.imageSource}`
+            fetch(SHARE_ART_URL, {
+                method: 'POST',headers: new Headers({
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }),
+                body: data
+            })
+            .then(response => response.json())
+            .then(data => {
+                recaptchaRef.current.reset()
+                this.setState({
+                    disabled: true,
+                    imageSent: data.recieved
+                })
             })
         })
     }
     render () {
         return (
-            <div className="mainContainer">
-                <p>Share your image with us.</p>
-                <p>Provide your name, image title, the link of the image itself and the source of it.</p>
-                <p>This is a SFW website, any kind of NSFW will be dismissed.</p>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="inputContainer">
-                        <input
-                            type="text"
-                            name="image-artist"
-                            value={this.state.imageArtist}
-                            onChange={e => this.setState({imageArtist: e.target.value })}
-                            placeholder="Artist name" />
-                    </div>
-                    <div className="inputContainer">
-                        <input
-                            type="text"
-                            name="image-title"
-                            value={this.state.imageTitle}
-                            onChange={e => this.setState({imageTitle: e.target.value })}
-                            placeholder="Image title" />
-                    </div>
-                    <div className="inputContainer">
-                        <input
-                            type="text"
-                            name="image-url"
-                            value={this.state.imageUrl}
-                            onChange={e => this.setState({imageUrl: e.target.value })}
-                            placeholder="http://image-url.com/image.jpg" />
-                    </div>
-                    <div className="inputContainer">
-                        <input
-                            type="text"
-                            name="image-source"
-                            value={this.state.imageSource}
-                            onChange={e => this.setState({imageSource: e.target.value })}
-                            placeholder="http://image-url.com" />
-                    </div>
-                    <Button handleClick={() => null} text={'Share image'} isDisabled={this.state.disabled} />
-                </form>
+            <div className="mainContainer row">
+                <div className="shareStatus">
+                    <p>Share your image with us.</p>
+                    <p>Provide your name, image title, the link of the image itself and the source of it.</p>
+                    <p>This is a SFW website, any kind of NSFW will be dismissed.</p>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="inputContainer">
+                            <input
+                                type="text"
+                                name="image-artist"
+                                value={this.state.imageArtist}
+                                onChange={e => this.setState({imageArtist: e.target.value })}
+                                placeholder="Artist name" />
+                        </div>
+                        <div className="inputContainer">
+                            <input
+                                type="text"
+                                name="image-title"
+                                value={this.state.imageTitle}
+                                onChange={e => this.setState({imageTitle: e.target.value })}
+                                placeholder="Image title" />
+                        </div>
+                        <div className="inputContainer">
+                            <input
+                                type="text"
+                                name="image-url"
+                                value={this.state.imageUrl}
+                                onChange={e => this.setState({imageUrl: e.target.value })}
+                                placeholder="http://image-url.com/image.jpg" />
+                        </div>
+                        <div className="inputContainer">
+                            <input
+                                type="text"
+                                name="image-source"
+                                value={this.state.imageSource}
+                                onChange={e => this.setState({imageSource: e.target.value })}
+                                placeholder="http://image-url.com" />
+                        </div>
+                        <div className="inputContainer">
+                            <ReCAPTCHA
+                                sitekey={KEY}
+                                onChange={() => {
+                                    this.setState({disabled: false})
+                                    }
+                                }
+                                ref={recaptchaRef} />
+                        </div>
+                        <Button handleClick={() => null} text={'Share image'} isDisabled={this.state.disabled} />
+                    </form>
+                </div>
+                <div className="shareStatus">
+                    {
+                        this.state.imageSent === null
+                            ? <div>
+                                We are ready to send the image...
+                            </div>
+                            : this.state.imageSent === "OK"
+                                ? <div>Your image was sent!</div>
+                                : this.state.imageSent === "KO"
+                                    ? <div>We could not send your image =(</div>
+                                    : <div>We are sending the image...</div>
+                    }
+                </div>
             </div>
         )
     }
